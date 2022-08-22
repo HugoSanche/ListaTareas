9397d8c27223cd7e37e24aafdccbada7f55988b9
HugoSanche <hbmclaren@gmail.com>
2022-08-22 17:26:57 -0500

    Final tarea

--- app.js
+++ app.js
@@ -4,54 +4,39 @@ const bodyParser = require("body-parser");
 
 const app =express();
 
+var item="";
+var items=["Buy Food","Cook Food","Eat Food"];
+//con este se puede obtener informacion de archivo html o del template ejs.
+//Como por ejemplo traer al informacion que puso el usuario de un input en el archivo html o template ejs
+app.use(bodyParser.urlencoded({extended:true}));
+//para utilizar el template ejs. En el arhivo html no se puede enviar info entonces se ocupan
+//template para enviar valores. el mas popular es ejs
 app.set('view engine','ejs');
 
 app.get("/", function(req, res){
   //res.send("Hello");
   var today=new Date();
-  var day=['Monday', 'Tuesday', 'Wednesday','Thursday', 'Friday','Saturday','Sunday'];
-  var currentDay = today.getDay();
-switch (currentDay)
-{
-case 0:
-    res.render("list",{kindofDay:day[0]});
-    break;
-case 1:
-    res.render("list",{kindofDay:day[1]});
-    break;
-case 2:
-    res.render("list",{kindofDay:day[2]});
-    break;
-case 3:
-    res.render("list",{kindofDay:day[3]});
-    break;
-case 4:
-    res.render("list",{kindofDay:day[4]});
-    break;
-case 5:
-    res.render("list",{kindofDay:day[5]});
-    break;
-case 6:
-    res.render("list",{kindofDay:day[6]});
-    break;
-    default:
-    console.log("El dia Octavo aun no existe");
-}
+  var options = {
+       weekday: "long",
+       year: "numeric",
+       month: "long",
+       day: "numeric"
+   };
 
+  var day=today.toLocaleDateString("es-MX",options)
 
-  // if (today.getDay()===6 || today.getDay()===0){
-  //   //res.write("<p>Que bien es dia de descanso</p>");
-  //   day="Weekend";
-  // }
-  // else{
-  //   day="Weekday";
-  //   // res.write("<p>el dia de hoy es</p>"+today.getDay());
-  //   // res.write("<h1>oh oh es dia de trabajo</h1>");
-  //   // res.send();
-  // }
-  // res.render("list",{kindofDay:day});
+res.render("list",{kindofDay:day,newListItems:items});
 });
 
 app.listen(3000,function(){
-  console.log("Servwer started on port 3000");
+  console.log("Server started on port 3000");
+});
+
+app.post("/",function(req, res){
+  //console.log(req.body.texto);
+   item =req.body.texto;
+   items.push(item);
+   //te redirige al inicio, pero en la variable item ya tiene el valor de "req.body.texto"
+   res.redirect("/");
+  //res.render("list",{newListItem:item});
 });
